6ab6a5790bac69e1f1bf64ae4004b726f62f64d3
lynnjiang <1596378726@qq.com>
2017-05-30 21:45:13 +0800

    modified:   DualWedgeIntersaction.cpp

--- DoubleWedgeIntersaction/DoubleWedgeIntersaction/DualWedgeIntersaction.cpp
+++ DoubleWedgeIntersaction/DoubleWedgeIntersaction/DualWedgeIntersaction.cpp
@@ -1,5 +1,4 @@
 #include "DualWedgeDefinations.h"
-
 using namespace std;
 
 void testPointLocation(Polyhedron& dualRegion) {
@@ -33,21 +32,207 @@ void testCase() {
 	//testPointLocation(dualRegion);
 }
 
+/* 
+Added by Liu
+Judge whether input segments are parallel or not, is yes reutrn true, otherwise return false;
+IsVertical: true if input segments are all vertical to x-axis, otherwise false;
+cosine_alpha: cosine of the angle (for coordinates rotation)
+sine_alpha: sine of the angle (for coordinates rotation)
+
+*/
+bool isParallel(vector<Segment> segments, bool& isVertical, double& cosine_alpha, double& sine_alpha)
+{
+	if (segments.size() < 2)
+	{
+		cout <<"Error: too few input segments"<<endl;
+		return false;
+	}
+	bool isPara = true;
+	Segment initialSeg = segments[0];
+	if (CGAL::to_double(initialSeg.first.x()) == CGAL::to_double(initialSeg.second.x()))
+		isVertical = true;
+	else
+		isVertical = false;
+	double slope = 0;
+	if (!isVertical)
+		slope = (CGAL::to_double(initialSeg.first.y()) - CGAL::to_double(initialSeg.second.y())) / (CGAL::to_double(initialSeg.first.x()) - CGAL::to_double(initialSeg.second.x()));
+	for (auto segment : segments)
+	{
+		bool curIsVertical = (CGAL::to_double(segment.first.x()) == CGAL::to_double(segment.second.x()));
+		if (curIsVertical)
+			if (isVertical)
+				continue;
+			else
+			{
+				isPara = false;
+				return isPara;
+			}
+		else
+		{
+			if (isVertical)
+			{
+				isPara = false;
+				return isPara;
+			}
+			else
+			{
+				double curSlope = (CGAL::to_double(segment.first.y()) - CGAL::to_double(segment.second.y())) / (CGAL::to_double(segment.first.x()) - CGAL::to_double(segment.second.x()));
+				if (curSlope == slope) // could be improved by tolerating eps error
+					continue;
+				else
+				{
+					isPara = false;
+					return isPara;
+				}
+			}
+		}
+	}
+	if (isPara)
+	{
+		/*
+		----------------------------------------------------------------------------------for reference-----------------------------------------------------------------------------------------------
+		alpha=theta-pi/2; where alpha is the angle to rotate the point (or the segment) clockwisely, theta is the angle between the segment and the x-axis and tangent(theta)=slope;
+	    so we have, cosine(alpha)=sine(theta) and sine(alpha)=-cosine(theta) ; 
+		Since tangent(theta)=slope, we have sine(theta)=slope / sqrt(1 + slope*slope); and cosine(theta)=1 / sqrt(1 + slope*slope);
+		----------------------------------------------------------------------------------end reference---------------------------------------------------------------------------------------------
+		*/
+		if (!isVertical)
+		{
+			cosine_alpha = slope / sqrt(1 + slope*slope);
+			sine_alpha = -1 / sqrt(1 + slope*slope);
+		}
+	}
+	return isPara;
+}
+/*
+Added by Liu
+Find the stabbing line for cases where segments are parallel.
+
+----------------------------begin reference---------------------------------------------------
+given a point p(x,y), rotate p around the origin by alpha angle clockwisely:
+
+x'=cosine(alpha)*x+sine(alpha)*y;
+y'=-sine(alpha)*x+cosine(alpha)*y;
+
+given a line : ax+by+c=0, rotate the line around the origin by alpha angle counterclockwisely:
+(a*cosine(alpha)-b*(sine(alpha)))x +(a*sine(alpha)+b*cosine(alpha))y+c=0;
+
+----------------------------end reference--------------------------------------------------------
+*/
+void getStabbingLinePara(Polyhedron& dualRegion, vector<Segment>& segments, Line&  stabbingLine, bool isVertical, double cosine_alpha, double sine_alpha)
+{
+	//The first step (rotation) : Rotate the coordinates to transform each segment (only if isVertical == false; otherwise do nothing)
+	if (!isVertical)
+	{
+		vector<Segment> segmentsTrans;
+		for (auto segment : segments)
+		{
+			double x1 = cosine_alpha*CGAL::to_double(segment.first.x()) + sine_alpha*CGAL::to_double(segment.first.y());
+			double y1 = -sine_alpha*CGAL::to_double(segment.first.x()) + cosine_alpha*CGAL::to_double(segment.first.y());
+			double x2 = cosine_alpha*CGAL::to_double(segment.second.x()) + sine_alpha*CGAL::to_double(segment.second.y());
+			double y2 = -sine_alpha*CGAL::to_double(segment.second.x()) + cosine_alpha*CGAL::to_double(segment.second.y());
+			if (x1 != x2)
+			{
+				cout << "Error: difference of x coordinates: " << abs(x1 - x2) << ", segment is not vertical after transformation, check cosine_alpha and sine_alpha." << endl;
+				return;
+			}
+			segmentsTrans.push_back(make_pair(Point(x1, y1), Point(x2, y2)));
+		}
+		segments = segmentsTrans;
+	}
+
+	//The second step (intersection) : Call addGap() to obtain band area for each transformed segment (vertical segment now), then call explorePolyhedron() to find stabbing point that is within the intersection of the band areas, thus find the stabbing line.
+	Point point1, point2;
+	for (auto segment : segments) {
+		point1 = segment.first;
+		point2 = segment.second;
+		//sort by x axis make sure the double wedge/ gap runs anticlockwise
+     	if (point1.y() < point2.y()) {
+			addGap(dualRegion, Line(-point1.x(), 1, point1.y()), Line(-point2.x(), 1, point2.y()));
+		}
+		else  if (point1.y() > point2.y()) {
+			addGap(dualRegion, Line(-point2.x(), 1, point2.y()), Line(-point1.x(), 1, point1.y()));
+		}
+		if (dualRegion.is_empty())
+		{
+			cout << "Error: empty intersected region." << endl;
+			return ;
+		}
+	}	
+	bool isfind = false;
+    Point stabbingPoint;
+	explorePolyhedron(dualRegion, stabbingPoint);
+	Explorer e = dualRegion.explorer();
+	for (auto v = e.vertices_begin(); v != e.vertices_end(); ++v) {
+		if (e.is_standard(v)) {
+			isfind = true;
+			stabbingPoint = e.point(v);
+			//cout << CGAL::to_double(stabbingPoint.x()) << " " << CGAL::to_double(stabbingPoint.y()) << endl;
+			break;
+		}
+	}
+	if (isfind) 
+		stabbingLine = Line(-stabbingPoint.x(), 1, stabbingPoint.y()); 
+	else 
+	{ 
+		cout << "Error: region is non-empty, but stabbing point not found" << endl; 
+		return;
+	}
+
+	//The Third step (inverse rotation) :Inversely rotate the coordinates to get the stabbing line transformed back to origianl coordinates. (only if isVertical == false; otherwise do nothing)
+	if (!isVertical)
+	{		
+		double a = stabbingLine.a;
+		double b = stabbingLine.b;
+		double c = stabbingLine.c;
+		double aa = a*cosine_alpha - b*sine_alpha;
+		double bb = a*sine_alpha + b*cosine_alpha;
+		double cc = c;
+		if (bb == 0)
+		{
+			cout << "Error: stabbling line is vertical to the x-axis, this case cannot be handled here" << endl;
+			return;
+		}
+		aa = aa / bb;
+		cc = cc / bb;
+		stabbingLine = Line(aa, 1, cc);
+		stabbingPoint = Point(-aa, cc);
+	}
+	cout << CGAL::to_double(stabbingPoint.x()) << " " << CGAL::to_double(stabbingPoint.y()) << endl;
+	return;
+}
 void runFromFile() {
 	Polyhedron dualRegion(Polyhedron::COMPLETE);
 	vector<Segment> segments;
 	Line stabbingLine;
 	ifstream ifs("test.data");
 	getSegmentsFromStream(ifs, segments);
-	getStabbingLine(dualRegion, segments, stabbingLine);
+	/*
+	Added by Liu: 
+	if isParallel() returns true, the program goes to getStabbingLinePara(), otherwise it goes to getStabbingLine();
+	*/
+	bool isVertical = false;
+	double cosine_alpha, sine_alpha;
+	if (isParallel(segments, isVertical, cosine_alpha, sine_alpha))
+		getStabbingLinePara(dualRegion, segments, stabbingLine, isVertical, cosine_alpha, sine_alpha);
+	else
+		getStabbingLine(dualRegion, segments, stabbingLine);
 }
-
 void runFromConsole() {
 	Polyhedron dualRegion(Polyhedron::COMPLETE);
 	vector<Segment> segments;
 	Line stabbingLine;
 	getSegmentsFromStream(cin, segments);
-	getStabbingLine(dualRegion, segments, stabbingLine);
+	/*
+	Added by Liu:
+	if isParallel() returns true, the program goes to getStabbingLinePara(), otherwise it goes to getStabbingLine();
+	*/
+	bool isVertical = false;
+	double cosine_alpha, sine_alpha;
+	if (isParallel(segments, isVertical, cosine_alpha, sine_alpha))
+		getStabbingLinePara(dualRegion, segments, stabbingLine, isVertical, cosine_alpha, sine_alpha);
+	else
+		getStabbingLine(dualRegion, segments, stabbingLine);
 }
 
 void runFromArgc(int argc, char** argv) {
@@ -63,7 +248,16 @@ void runFromArgc(int argc, char** argv) {
 	while (ss >> p1x >> p1y >> p2x >> p2y) {
 		segments.push_back(make_pair(Point(p1x, p1y), Point(p2x, p2y)));
 	}
-	getStabbingLine(dualRegion, segments, stabbingLine);
+	/*
+	Added by Liu:
+	if isParallel() returns true, the program goes to getStabbingLinePara(), otherwise it goes to getStabbingLine();
+	*/
+	bool isVertical = false;
+	double cosine_alpha, sine_alpha;
+	if (isParallel(segments, isVertical, cosine_alpha, sine_alpha))
+		getStabbingLinePara(dualRegion, segments, stabbingLine, isVertical, cosine_alpha, sine_alpha);
+	else
+		getStabbingLine(dualRegion, segments, stabbingLine);
 }
 
 bool getStabbingLine(Polyhedron& dualRegion, vector<Segment>& segments, Line& stabbingLine) {
